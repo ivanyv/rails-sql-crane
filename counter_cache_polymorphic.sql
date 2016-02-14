a2db1bb9031ec62474b3f1ff7aa0cd636cc86bba
Ivan Vega <ivanyv@users.noreply.github.com>
2016-02-14 01:08:47 -0600

    Create counter_cache_polymorphic.sql

---
+++ counter_cache_polymorphic.sql
@@ -0,0 +1,14 @@
+-- Use `counter_cache.sql` and these triggers for polymorphic associations:
+CREATE TABLE comments_p (id integer, commentable_id integer, commentable_type character varying);
+
+CREATE TRIGGER update_posts_comments_count
+  AFTER INSERT OR UPDATE ON comments_p
+  FOR EACH ROW
+  WHEN (NEW.commentable_type = 'Post')
+  EXECUTE PROCEDURE update_counter_cache('posts', 'comments_count', 'commentable_id');
+
+CREATE TRIGGER update_posts_comments_count_on_delete
+  AFTER DELETE ON comments_p
+  FOR EACH ROW
+  WHEN (OLD.favoriteable_type = 'Post')
+  EXECUTE PROCEDURE update_counter_cache('posts', 'comments_count', 'commentable_id');
